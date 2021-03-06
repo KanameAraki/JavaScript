d94008789d98d3d3b0d820711cd52d0d0d62dbac
KanameAraki <kana911me@gmail.com>
2021-03-06 17:51:48 +0900

    [Finish]c2

---
+++ c1/app.js
@@ -0,0 +1,30 @@
+// var hello = "Hello World"
+// alert(hello);
+// var int1 = 1;
+// var int2 = -10;
+
+// var orange = 100;
+// var apple = 120;
+// if (orange < apple) {
+//   alert("みかんはりんごより安い");
+// } else if (orange == apple) {
+//   alert("みかんとりんごは同じ");
+// } else {
+//   alert("みかんはりんごより高い");
+// }
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+// while (num < max) {
+//   num = num * 2
+//   count = count + 1;
+// }
+// alert("２をかけ続けて" + max + "を超える回数は" + count + "回です");
+
+var i;
+var num = 0;
+for (i = 1; i < 11; i++) {
+  num = num + i;
+}
+alert("１から１０まで足算した結果は" + num + "です");
