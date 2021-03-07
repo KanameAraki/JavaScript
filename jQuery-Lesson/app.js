c9f15590ad49171aca82e882799222bf1a33c57f
KanameAraki <kana911me@gmail.com>
2021-03-07 09:51:31 +0900

    [Finish]c5

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,40 @@
-$(document).ready(function () {
-  $("body").html("<h1>Hello jQUery!!</h1>");
+// $(document).ready(function () {
+//   $("body").html("<h1>Hello jQUery!!</h1>");
+// });
+
+// $(function () {
+//   $(".box1").css({
+//     "background-color": "#0000FF",
+//     "height": "100px"
+//   });
+// });
+
+// $(function () {
+//   $(".box1").slideDown();
+// });
+
+// $(function () {
+//   $(".box1").slideUp();
+// });
+
+// $(function () {
+//   $(".box1").show();
+//   $(".box1").css({ "background-color": "#0000FF" });
+// });
+
+// $(function () {
+//   $(".box1").hide();
+// });
+
+
+
+
+$(function () {
+  $(".box1").slideDown(function () {
+    $(".box1").css({
+      "background-color": "#0000FF",
+      "height": "100px",
+      "width": "200px"
+    }).slideUp();
+  });
 });
